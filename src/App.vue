cdcadaec767b61bd882e355224ca7114a3c4726f
Yudai Watanabe <yudai.0702wa@gmail.com>
2021-09-06 20:03:09 +0900

    クリア機能とvideojsいんすとる

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
     <img alt="Title" src="./assets/title.png" />
+    <!-- <div>
+      <video-player :options="videoOptions">
+        <source src="/media/examples/flower.mp4" type="video/mp4" />
+      </video-player>
+    </div> -->
     <LoLanePicker />
   </div>
 </template>
 
 <script>
 import LoLanePicker from "./components/MainScreen.vue";
+// import VideoPlayer from "@/components/VideoPlayer.vue";
+// import "video.js/dist/video-js.css";
 
 export default {
   name: "App",
   components: {
+    // VideoPlayer,
     LoLanePicker,
   },
+  // data() {
+  //   return {
+  //     videoOptions: {
+  //       autoplay: true,
+  //       controls: true,
+  //       sources: [
+  //         {
+  //           src: "/assets/godfreeze.mp4",
+  //           type: "video/mp4",
+  //         },
+  //       ],
+  //     },
+  //   };
+  // },
 };
 </script>
 
--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -15,7 +15,7 @@
       </div>
 
       <span class="Samoner">サモナーネームを入力：</span>
-      <input v-model="item" />
+      <input id="form1" v-model="item" />
       <button v-bind:disabled="isPush" v-on:click="add_item()">追加</button>
     </div>
     <div class="box1">
@@ -72,23 +72,41 @@
       <br />
     </div>
 
-    <li v-for="(item, index) in itemsBef" :key="index">
-      {{ item }}
-      <button v-on:click="del_item(index)">x</button>
-    </li>
-    <div>
+    <div class="box1">
+      <li v-for="(item, index) in itemsBef" :key="index">
+        {{ item }}
+        <button v-on:click="del_item(index)">x</button>
+      </li>
+
       <button v-on:click="lane_pick()">レーンを決める</button>
-      <div class="box1">
-        <ul id="nav">
-          <div>
-            <p class="top">トップ：{{ lane[0].name }}</p>
-            <p class="jungle">ジャングル：{{ lane[1].name }}</p>
-            <p class="mid">ミッド：{{ lane[2].name }}</p>
-            <p class="support">サポート：{{ lane[3].name }}</p>
-            <p class="adc">ボット：{{ lane[4].name }}</p>
+      <button v-on:click="clean_data()">サモナー以外リセット</button>
+    </div>
+
+    <div class="box2">
+      <ul id="nav">
+        <div>
+          <div class="text">
+            <img class="top" src="../assets/TOP.png" />
+            トップ：{{ lane[0].name }}
           </div>
-        </ul>
-      </div>
+          <div class="text">
+            <img class="jungle" src="../assets/JUNGLE.png" />
+            ジャングル：{{ lane[1].name }}
+          </div>
+          <div class="text">
+            <img class="middle" src="../assets/MIDDLE.png" />
+            ミッド：{{ lane[2].name }}
+          </div>
+          <div class="text">
+            <img class="support" src="../assets/SUPPORT.png" />
+            サポート：{{ lane[3].name }}
+          </div>
+          <div class="text">
+            <img class="adc" src="../assets/ADC.png" />
+            ボット：{{ lane[4].name }}
+          </div>
+        </div>
+      </ul>
     </div>
   </div>
 </template>
@@ -117,21 +135,36 @@ export default {
     };
   },
 
+  watch: {
+    selectedPlayers: function () {
+      //プレイヤー人数が前と大きくなったときと少なくなったときの動作
+      if ((this.itemsBef.length <= this, this.selectedPlayers)) {
+        this.isPush = false;
+        this.clean_data();
+      } else {
+        this.isPush = false;
+        this.items = [];
+        this.itemsBef = [];
+        this.clean_data();
+      }
+    },
+  },
+
   methods: {
     add_item: function () {
-      console.log(this.items.length);
-      console.log(this.selectedPlayers);
       if (this.itemsBef.length < this.selectedPlayers) {
         this.itemsBef.push(this.item);
         //野良の人数分のチェックしたかの確認
         if (this.itemsBef.length > this.selectedPlayers - 1) this.isPush = true;
       }
-      console.log(this.itemsBef);
+
+      this.item = []; //値追加後に入力欄を空にする
     },
     del_item: function (_index) {
       this.itemsBef.splice(_index, 1);
     },
     lane_pick: function () {
+      //itemsにitemsBefの配列をコピー
       this.items = this.itemsBef.slice();
 
       //Randomにitem配列の順番を変える処理
@@ -149,7 +182,6 @@ export default {
           }
         }
       }
-      console.log(this.items);
 
       for (let i = 0; i < 5; i++) {
         if (this.lane[i].name == null && this.items.length >= this.arrCount) {
@@ -157,7 +189,7 @@ export default {
           this.arrCount++;
         }
       }
-      console.log("itemsBef:" + this.itemsBef);
+      this.arrCount = 0;
     },
     canCheck: function () {
       if (this.selectedPlayers == 5) {
@@ -166,6 +198,16 @@ export default {
         this.isChecked = true;
       }
     },
+    clean_data: function () {
+      //野良が行くレーンと振り当てられたレーンの配列を消す
+      this.checkedNoraLane = [];
+      for (let i = 0; i < 5; i++) {
+        this.lane[i].name = null;
+      }
+
+      //チェックできるようにする
+      this.isChecked = false;
+    },
   },
 };
 </script>
@@ -173,51 +215,61 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .box1 {
-  padding: 0.5em 1em 1em;
-  margin: 2em 0;
+  text-align: center;
+  padding: 10px 10px 10px;
+  margin: 10px 10px 10px;
   font-weight: bold;
   border: solid 3px #000000;
+  font-size: 20px;
 }
 .box1 p {
   margin: 0;
   padding: 20;
 }
+.box2 {
+  padding: 10px 10px 10px;
+  margin: 10px 10px 10px;
+  font-weight: bold;
+  border: solid 3px #000000;
+}
 .size {
-  padding: 0.5em 10em 0.5em;
+  padding: 10px 0px 0.5em;
+  margin: 10px 600px 20px;
+}
+
+.text {
+  padding-right: 2em;
+  padding-left: 2em;
+  text-align: left;
+  font-size: 40px;
 }
 
 .top {
-  padding-left: 1px;
-  line-height: 50px;
-  background: url(../assets/TOP.png) no-repeat;
-  background-size: 3%;
+  vertical-align: middle;
+  padding-right: 80px;
+  width: 90px;
 }
 
 .jungle {
-  padding-left: 50px;
-  line-height: 50px;
-  background: url(../assets/JUNGLE.png) no-repeat;
-  background-size: 3%;
+  vertical-align: middle;
+  width: 90px;
 }
 
-.mid {
-  padding-left: 50px;
-  line-height: 50px;
-  background: url(../assets/MIDDLE.png) no-repeat;
-  background-size: 3%;
+.middle {
+  vertical-align: middle;
+  padding-right: 80px;
+  width: 90px;
 }
 
 .support {
-  padding-left: 50px;
-  line-height: 50px;
-  background: url(../assets/SUPPORT.png) no-repeat;
-  background-size: 3%;
+  vertical-align: middle;
+  padding-right: 40px;
+  width: 90px;
 }
 
 .adc {
-  padding-left: 50px;
-  line-height: 50px;
-  background: url(../assets/ADC.png) no-repeat;
-  background-size: 3%;
+  vertical-align: middle;
+  padding-right: 80px;
+  width: 90px;
 }
 </style>
